308fe9774aa058f19d5ade1987215258f23c5609
Jamey Huffnagle <mjhuffnagle@gmail.com>
2023-03-14 12:13:41 -0400

    Deleted all files

--- client/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import statReducer from './stateSlice';
-
-// this only works for one level of reducers
-export const store = configureStore({
-  reducer: {
-    updater: statReducer
-  }
-});
--- server/routes/weatherRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const weatherController = require('../controller/weatherController');
-
-const router = express.Router();
-
-router.post('/', weatherController.getData, (req, res) => res.status(200).json(res.locals.response));
-
-module.exports = router;
